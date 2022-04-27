b8bbf55825e231c0f0c153c0ed0e26f84b126cf6
Kamila <kamkopiczko@gmail.com>
2022-04-27 21:20:03 +0200

    created path operations for creating and retrievieng a user from a database

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends, status
-from .. import schemas
-from .. import models
+from fastapi import APIRouter, Depends, status, HTTPException
+from .. import schemas, models, utils
 from sqlalchemy.orm import Session
 from ..database import get_db
 
@@ -8,3 +7,27 @@ router = APIRouter(
     prefix="/users",
     tags=['Users']
 )
+
+@router.post("/")
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    user_with_hashed_password = {"email": user.email, "hashed_password": hashed_password}
+    # user.hashed_password = hashed_password
+
+    new_user = models.User(**user_with_hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.get("/")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+@router.get("/{id}")
+def get_user_by_id(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with an id {id} does not exist')
+    return user
