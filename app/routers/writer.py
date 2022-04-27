73cfef9383a43d93a15b5c1451146ebb44c40be6
Kamila <kamkopiczko@gmail.com>
2022-04-27 22:56:48 +0200

    path operations retrieving a writer from a db

--- app/routers/writer.py
+++ app/routers/writer.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from .. import schemas
 from .. import models
 from sqlalchemy.orm import Session
@@ -9,8 +9,8 @@ router = APIRouter(
     tags=['Writers']
 )
 
-@router.post("/", response_model=schemas.Writer)
-def create_user(writer: schemas.WriterCreate, db: Session = Depends(get_db)):
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Writer)
+def create_writer(writer: schemas.WriterCreate, db: Session = Depends(get_db)):
     
     new_writer = models.Writer(**writer.dict())
     db.add(new_writer)
@@ -18,3 +18,14 @@ def create_user(writer: schemas.WriterCreate, db: Session = Depends(get_db)):
     db.refresh(new_writer)
     return new_writer
 
+@router.get("/", response_model=list[schemas.Writer])
+def get_writers(db: Session = Depends(get_db)):
+    writers = db.query(models.Writer).all()
+    return writers
+
+@router.get("/{id}", response_model=schemas.Writer)
+def get_writer_by_id(id: int, db: Session = Depends(get_db)):
+    writer = db.query(models.Writer).filter(models.Writer.id == id).first()
+    if not writer:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Writer with an id {id} does not exist')
+    return writer
