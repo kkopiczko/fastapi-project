e894822eb6ce453ff16c160815663d461278696c
Kamila <kamkopiczko@gmail.com>
2022-04-27 22:42:19 +0200

    path operation adding a writer to a db

--- app/routers/writer.py
+++ app/routers/writer.py
@@ -8,3 +8,13 @@ router = APIRouter(
     prefix="/writers",
     tags=['Writers']
 )
+
+@router.post("/", response_model=schemas.Writer)
+def create_user(writer: schemas.WriterCreate, db: Session = Depends(get_db)):
+    
+    new_writer = models.Writer(**writer.dict())
+    db.add(new_writer)
+    db.commit()
+    db.refresh(new_writer)
+    return new_writer
+
