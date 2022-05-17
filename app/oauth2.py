52a2c89207e9011d5c4a0b0c34c864ab36b0df95
Kamila <kamkopiczko@gmail.com>
2022-05-17 16:16:12 +0200

    added constants needed for creating a JWT token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,3 +1,9 @@
+from jose import JWTError, jwt
+
+SECRET_KEY = "803e903091e47123cfe681be4804db35c4002d64507874cb951ff60e253a5917"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
