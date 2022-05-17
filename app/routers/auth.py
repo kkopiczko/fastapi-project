58e19d5e56e500a2030b6a85efb6e75819f12464
Kamila <kamkopiczko@gmail.com>
2022-05-17 16:41:07 +0200

    we use OAuth2PasswordRequestForm to receive user credentials from the client

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from ..database import get_db
@@ -9,9 +9,9 @@ from .. import schemas, models, utils, oauth2
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserCreate, db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
@@ -21,5 +21,5 @@ def login(user_credentials: schemas.UserCreate, db: Session = Depends(get_db)):
 
     # create token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
-    
+
     return {"access_token": access_token, "token_type": "bearer"}
