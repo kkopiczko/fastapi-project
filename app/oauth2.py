e38485c4beb674091b138f99c688d658fd8113af
Kamila <kamkopiczko@gmail.com>
2022-05-17 16:23:56 +0200

    function that returns a jwt token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,20 @@
 from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "803e903091e47123cfe681be4804db35c4002d64507874cb951ff60e253a5917"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-from fastapi.security import OAuth2PasswordBearer
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    jwt_encoded = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return jwt_encoded
+
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
